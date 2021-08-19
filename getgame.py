885bb849dcc173479b90630e7a8e175309ed67f7
龙际超 <longjichao@hemeiloan.com>
2021-08-19 15:13:28 +0800

    v0.0.5

--- getgame.py
+++ getgame.py
@@ -11,8 +11,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 
-
-
 def is_chinese(string):
     """
     检查整个字符串是否包含中文
@@ -24,7 +22,6 @@ def is_chinese(string):
             return True
 
     return False
-
 
 class Spider(object):
 
@@ -38,7 +35,7 @@ class Spider(object):
 
     def get_team_odd_oddslist(self, team_data):
         url = 'http://op1.win007.com/oddslist/' + team_data[0] + '.htm'
-        #print('get_team_odd_oddslist:' + url)
+        print('get_team_odd_oddslist:' + url)
         # 打开chrome浏览器（需提前安装好chromedriver）
         browser = webdriver.Chrome()
         #print("正在打开网页...")
@@ -142,7 +139,8 @@ class Spider(object):
         soup = BeautifulSoup(browser.page_source, "lxml")
         browser.close()
 
-        oddurl = 0
+        oddurl = "http://vip.win007.com"
+
         asiantrs = soup.findAll('tr')
         for asiantr in asiantrs:
             asiantds = asiantr.findAll('td')
@@ -152,9 +150,10 @@ class Spider(object):
                 for asiantd in asiantds:
                     if asiantd.text.find('详') >= 0:
                         asianas = asiantd.findAll('a')
-                        oddurl = "http://vip.win007.com" + asianas[0]['href']
+                        oddurl += asianas[0]['href']
                         break
                 break
+            #print(oddurl)
         return oddurl
 
     def get_team_odd_asianHistory(self, team_data, oddurl):
@@ -167,7 +166,7 @@ class Spider(object):
         #print("等待网页响应...")
         # 需要等一下，直到页面加载完成
         wait = WebDriverWait(browser, 10)
-        wait.until(EC.presence_of_element_located((By.ID, "odds2")))
+        wait.until(EC.presence_of_element_located((By.ID, "odds")))
 
         #print("正在获取网页数据...")
         soup = BeautifulSoup(browser.page_source, "lxml")
@@ -198,37 +197,32 @@ class Spider(object):
 
     def get_team_asian_data(self, team_data):
         oddurl = self.get_team_asian_asianlist(team_data)
-        if oddurl == 0:
-            return 0
-        else:
-            self.get_team_odd_asianHistory(team_data, oddurl)
+        self.get_team_odd_asianHistory(team_data, oddurl)
         return 1
     # =========================================================================================================
 
     def get_team_data(self, team_data):
-        if self.get_team_odd_data(team_data) == 0:
+        reda = self.get_team_odd_data(team_data)
+        if reda == 0:
             return 0
-        if self.get_team_asian_data(team_data) == 0:
-            return 0
+        self.get_team_asian_data(team_data)
         del team_data[0]
         for i, value in enumerate(team_data):
-            if (6 <= i <= 12) or (14 <= i <= 15) or i == 17:
+            if (4 <= i <= 10) or (12 <= i <= 13) or i == 15:
                 value = float(value)
-            elif i == 3 or i == 4:
-                value = int(value)
             else:
                 value = str(value.encode('utf-8'))
         return team_data
 
-    def get_team_ids(self, date_str):
-        main_url = 'http://jc.win007.com/schedule.aspx?d=' + date_str
+    def get_team_ids(self, main_url):
         #print(main_url)
-        date_s = datetime.strptime(date_str, '%Y-%m-%d')
         self.driver.get(main_url)
         teams = self.driver.find_elements_by_xpath("//*[@id='table_live']/tbody/tr")
         data = []
         for team in teams:
             team_id = team.get_attribute("id")
+            if len(team_id) == 0:
+                continue
             index1 = team_id.find('tr1')
             if index1 <= -1:
                 continue
@@ -236,59 +230,47 @@ class Spider(object):
             del team_id[0]
             team_name = team.text
             index1 = team_name.find('亚欧析')
-            index2 = team_name.find('完')
-            if index1 > -1 and index2 > -1:
+            if index1 > -1:
                 team_name = team_name[0:index1].split()
-                team_name = team_name[1:8]
-                del team_name[2]
-                del team_name[4]
-                team_name_score = team_name[3].split('-')
-                team_name[3] = team_name_score[0]
-                team_name.insert(4, team_name_score[1])
-                #print(team_id, team_name)
-                if team_name[1] >= '16:00':
-                    team_name[1] = date_str + ' ' + team_name[1]
-                else:
-                    datetime_object = (date_s + timedelta(days=1)).strftime("%Y-%m-%d")
-                    team_name[1] = str(datetime_object) + ' ' + team_name[1]
-                data.append([team_id + team_name])
+                if is_chinese(team_name[3]):
+                    team_name = team_name[1:6]
+                    del team_name[3]
+                    #print(team_id, team_name)
+                    data_s = datetime.now().date()
+                    if team_name[1] >= '16:00':
+                        team_name[1] = str(data_s) + ' ' + team_name[1]
+                    else:
+                        datetime_object = (data_s+ timedelta(days=1)).strftime("%Y-%m-%d")
+                        team_name[1] = datetime_object + ' ' + team_name[1]
+                    data.append([team_id + team_name])
         self.team_list = data
         # self.team_list = pd.DataFrame(data, columns=['team_name', 'team_id'])
         # self.team_list.to_excel('国家队ID.xlsx', index=False)
 
     def get_all_team_data(self):
-        # 循环爬取每一支队的比赛数据
-
-        # 先通过世界杯主页获取所有32只队的ID（构成球队URL）
-
-        datestartstr = '2021-06-06'
-        dateendstr = '2021-06-06'
-        datestart = datetime.strptime(datestartstr, '%Y-%m-%d')
-        dateend = datetime.strptime(dateendstr, '%Y-%m-%d')
-
+        # 循环爬取每一支队的比赛数据(URL)
         datas = []
-        while datestart <= dateend:
-            data = []
-            self.get_team_ids(datestart.strftime('%Y-%m-%d'))
-            for i, [team_data] in enumerate(self.team_list):
-                print(i, team_data)
-                reda = self.get_team_data(team_data)
-                if reda == 0:
-                    continue
-                data.append(team_data)
-            print('=========================================================')
-            #print(data)
-            df = pd.DataFrame(data,
-                              columns=['赛事', '时间', '主队', '主队进球', '客队进球', '客队', '初胜赔', '初平赔', '初负赔', '终胜赔', '终平赔', '终负赔',
-                                       '初上盘水', '初盘口', '初下盘水', '终上盘水', '终盘口', '终下盘水'])
-            datas.append(df)
-            datestart += timedelta(days=1)
+        data = []
+        self.get_team_ids('http://jc.win007.com/index.aspx')
+        for i, [team_data] in enumerate(self.team_list):
+            #print(i, team_data)
+            reda = self.get_team_data(team_data)
+            if reda == 0:
+                continue
+            data.append(team_data)
+        print('=========================================================')
+        print(data)
+        df = pd.DataFrame(data,
+                          columns=['赛事', '时间', '主队', '客队', '初胜赔', '初平赔', '初负赔', '终胜赔', '终平赔', '终负赔',
+                                   '初上盘水', '初盘口', '初下盘水', '终上盘水', '终盘口', '终下盘水'])
+        datas.append(df)
+
         self.driver.close()
         output = pd.concat(datas)
         output.reset_index(drop=True, inplace=True)
         # print(output)
-        output.to_csv('data_'+ datestartstr + '_' + dateendstr + '.csv', index=False, encoding='utf-8')
-        print(datestartstr + 'to' + dateendstr + 'over')
+        output.to_csv('预测_' + str(datetime.now().date()) + '.csv', index=False, encoding='utf-8')
+        print('over')
 
 if __name__ == "__main__":
     spider = Spider()
